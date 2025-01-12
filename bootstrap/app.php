af3aab936d16c5c567d7a7bfce413cb1cd7fc950
Didier Berlioz <berliozd@gmail.com>
2025-01-12 22:43:20 +0100

    Add logo, change color, fix middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,9 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-
-    })->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,7 +7,7 @@ import Welcome from '@/Components/Welcome.vue';
     <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Here is the Dashboard
+                Dashboard
             </h2>
         </template>
 
