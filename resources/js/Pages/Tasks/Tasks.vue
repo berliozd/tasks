e5f6e2772656d95f5cc8964747ae413730babe66
Didier Berlioz <berliozd@gmail.com>
2025-01-15 17:35:26 +0100

    Remove padding on tasks page

--- resources/js/Pages/Tasks/Tasks.vue
+++ resources/js/Pages/Tasks/Tasks.vue
@@ -108,7 +108,7 @@ const formatDateTime = (date) => {
         <template #header>
             <h2 class="font-semibold text-xl leading-tight">Tasks</h2>
         </template>
-        <div class="py-12">
+        <div class="">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="min-h-6 ">
                     <SavedLabel/>
