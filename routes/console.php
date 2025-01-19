aa4ad05a444864ed3c93863b2f1995d8b7af6496
Didier Berlioz <berliozd@gmail.com>
2025-01-19 22:26:57 +0100

    Fix job

--- routes/console.php
+++ routes/console.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Schedule;
 
 Log::info('console run');
 
-Schedule::job(new ProcessPodcast())->everyMinute();
+Schedule::job(new ProcessPodcast)->everyMinute();
 
 Log::info('console end');
