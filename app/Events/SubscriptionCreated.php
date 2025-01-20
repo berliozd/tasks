43ac9dd0fa533343ecb365048ba476f12c37db20
Didier Berlioz <berliozd@gmail.com>
2025-01-20 13:16:14 +0100

    add missing implementation ShouldBroadcast on event

--- app/Events/SubscriptionCreated.php
+++ app/Events/SubscriptionCreated.php
@@ -4,11 +4,12 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class SubscriptionCreated
+class SubscriptionCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
