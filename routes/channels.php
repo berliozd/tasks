65f1fbffd33cd7f08172b65131472e883b99a941
Didier Berlioz <berliozd@gmail.com>
2025-01-21 14:49:32 +0100

    Fix channels routes to match the new names of teh channels

--- routes/channels.php
+++ routes/channels.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Log;
 
-Broadcast::channel('my-public-channel', function () {
-    return true; // Allow everyone to listen that channel
-});
+//Broadcast::channel('my-public-channel', function () {
+//    return true; // Allow everyone to listen that channel
+//});
 
 Broadcast::channel('my-private-channel', function ($user) {
     Log::info($user->id);
