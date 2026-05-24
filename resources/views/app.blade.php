64bdae62ed543069e95306d9455ca0ea4d759d04
Didier Berlioz <berliozd@gmail.com>
2026-05-24 12:24:32 +0200

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" type="image/png" href="/img/favicon-tasks.png">
+        <link rel="apple-touch-icon" href="/img/favicon-tasks.png">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
